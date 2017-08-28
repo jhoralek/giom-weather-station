8420d9a1e40c1f4cb0db88e09f7a25ef322754e1
Jiří Horálek <jiri.horalek@linksoft.cz>
2017-08-28 09:36:54 +0200

    new barch chart component

---
+++ src/components/BarChart.vue
@@ -0,0 +1,60 @@
+<script>
+import { Bar } from 'vue-chartjs'
+
+export default Bar.extend({
+    name: 'barChart',
+    props: {
+        chartName: {
+            type: String,
+            default: ''
+        },
+        dataTypeName: {
+            type: String,
+            default: ''
+        },
+        chartData: {
+            type: Array,
+            required: false
+        },
+        chartLabels: {
+            type: Array,
+            required: true
+        }        
+    },
+    data() {
+        return {
+            options: {
+                responsive: true,
+                legend: {
+                    display: true,
+                    fontColor: '#ffffff'                    
+                },                
+                title: {
+                    display: true,
+                    text: this.chartName,
+                    fontColor: '#ffffff'
+                }
+            }
+        }
+    },
+    mounted() {
+        this.render()
+    },
+    watch: {
+        chartLabels: function (val) {
+            this.render()
+        }
+    },
+    methods: {
+        render () {
+            this.renderChart({
+                labels: this.chartLabels,
+                datasets: [{
+                    label: this.dataTypeName,
+                    data: this.chartData
+                }]
+            }, this.options)
+        }
+    }
+})
+</script>
