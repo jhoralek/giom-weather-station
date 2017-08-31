c8c9daa165be9419b3ff0f4bd330e337bf5f7a13
Jiří Horálek <jiri.horalek@linksoft.cz>
2017-08-31 13:55:10 +0200

    implementing doughnut chart and use it for showing last mesured temperature

---
+++ src/components/DoughnutChart.vue
@@ -0,0 +1,49 @@
+<script>
+const baseColor = '#000000'
+
+import { Doughnut } from 'vue-chartjs'
+
+export default Doughnut.extend({
+  name: 'doughnutChart',
+  props: {
+    settings: {
+      type: Object,
+      required: true
+    }
+  },
+  data () {
+    return {
+      options: {
+        responsive: true,
+        legend: {
+          display: this.settings.legend || true,
+          fontColor: this.settings.fongColor || baseColor
+        },
+        animation: {
+          animateScale: true,
+          animateRotate: true
+        },
+        cutoutPercentage: 60,
+        rotation: Math.PI,
+        circumference: Math.PI
+      }
+    }
+  },
+  watch: {
+    settings: function (val) {
+      this.render()
+    }
+  },
+  mounted () {
+    this.render()
+  },
+  methods: {
+    render () {
+      this.renderChart({
+        labels: this.settings.labels,
+        datasets: this.settings.datasets
+      }, this.options)
+    }
+  }
+})
+</script>
--- src/components/LineChart.vue
+++ src/components/LineChart.vue
@@ -1,92 +1,94 @@
 
 <script>
- import { Line } from 'vue-chartjs'
+const baseColor = '#000000'
+
+import { Line } from 'vue-chartjs'
 
 export default Line.extend({
-   name: 'lineChart',
-   props: {
-     settings: {
-       type: Object,
-       required: true
-     }
-   },
-   data () {
-     return {
-       options: {
-         responsive: true,
-         title: {
-           display: true,
-           text: this.settings.name || '',
-           fontColor: this.settings.fontColor || '#000000'
-         },
-         legend: {
-           display: true,
-           labels: {
-             fontColor: this.settings.fontColor || '#000000'
-           }
-         },
-         tooltips: {
-           mode: 'index',
-           intersect: false
-         },
-         hover: {
-           mode: 'nearest',
-           intersect: true
-         },
-         scales: {
-           xAxes: [{
-             display: true,
-             scaleLabel: {
-               display: true,
-               labelString: this.settings.xLabelName || '',
-               fontColor: this.settings.fontColor || '#000000'
-             },
-             ticks: {
-               fontColor: this.settings.fontColor || '#000000'
-             }
-           }],
-           yAxes: [{
-             display: true,
-             scaleLabel: {
-               display: true,
-               labelString: this.settings.yLabelName || '',
-               fontColor: this.settings.fontColor || '#000000'
-             },
-             ticks: {
-               fontColor: this.settings.fontColor || '#000000',
-               min: -20,
-               max: 40,
-               stepSize: 5,
-               callback: this.settings.callback || function (val) { return val }
-             }
-           }]
-         }
-       }
-     }
-   },
-   watch: {
-     settings: function (val) {
-       this.render()
-     }
-   },
-   mounted () {
-     this.render()
-   },
-   methods: {
-     render () {
-       this.renderChart({
-         labels: this.settings.labels,
-         datasets: [{
-           label: this.settings.yLabelName || '',
-           borderColor: this.settings.borderColor || '#000000',
-           pointBackgroundColor: this.settings.pointBackground || '#000000',
-           borderwidth: this.settings.borderWidth || 0,
-           pointBordercolor: this.settings.pointBorderColor || '#000000',
-           backgroundColor: this.settings.background || 'transparent',
-           data: this.settings.data
-         }]
-       }, this.options)
-     }
-   }
+  name: 'lineChart',
+  props: {
+    settings: {
+      type: Object,
+      required: true
+    }
+  },
+  data () {
+    return {
+      options: {
+        responsive: true,
+        title: {
+          display: true,
+          text: this.settings.name || '',
+          fontColor: this.settings.fontColor || baseColor
+        },
+        legend: {
+          display: true,
+          labels: {
+            fontColor: this.settings.fontColor || baseColor
+          }
+        },
+        tooltips: {
+          mode: 'index',
+          intersect: false
+        },
+        hover: {
+          mode: 'nearest',
+          intersect: true
+        },
+        scales: {
+          xAxes: [{
+            display: true,
+            scaleLabel: {
+              display: true,
+              labelString: this.settings.xLabelName || '',
+              fontColor: this.settings.fontColor || baseColor
+            },
+            ticks: {
+              fontColor: this.settings.fontColor || baseColor
+            }
+          }],
+          yAxes: [{
+            display: true,
+            scaleLabel: {
+              display: true,
+              labelString: this.settings.yLabelName || '',
+              fontColor: this.settings.fontColor || baseColor
+            },
+            ticks: {
+              fontColor: this.settings.fontColor || baseColor,
+              min: -20,
+              max: 40,
+              stepSize: 5,
+              callback: this.settings.callback || function (val) { return val }
+            }
+          }]
+        }
+      }
+    }
+  },
+  watch: {
+    settings: function (val) {
+      this.render()
+    }
+  },
+  mounted () {
+    this.render()
+  },
+  methods: {
+    render () {
+      this.renderChart({
+        labels: this.settings.labels,
+        datasets: [{
+          label: this.settings.yLabelName || '',
+          borderColor: this.settings.borderColor || baseColor,
+          pointBackgroundColor: this.settings.pointBackground || baseColor,
+          borderwidth: this.settings.borderWidth || 0,
+          pointBordercolor: this.settings.pointBorderColor || baseColor,
+          backgroundColor: this.settings.background || 'transparent',
+          data: this.settings.data
+        }]
+      }, this.options)
+    }
+  }
 })
 </script>
